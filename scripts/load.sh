7db35a068a7ff77675536295520b267e87d0fe5f
kiran <kiran@neostack.com>
2017-01-31 00:04:45 +0530

    Npm script to load and clean db

--- scripts/load.sh
+++ scripts/load.sh
@@ -1,6 +1,6 @@
-mongoimport --db vod-development --collection categories --file ./db-data/categories.json --jsonArray
-mongoimport --db vod-development --collection equipment --file ./db-data/equipment.json --jsonArray
-mongoimport --db vod-development --collection ingredients --file ./db-data/ingredients.json --jsonArray
-mongoimport --db vod-development --collection learningpaths --file ./db-data/learningpaths.json --jsonArray
-mongoimport --db vod-development --collection chefs --file ./db-data/chefs.json --jsonArray
-mongoimport --db vod-development --collection tags --file ./db-data/tags.json --jsonArray
+mongoimport --db vod-development --collection categories --file ./scripts/db-data/categories.json --jsonArray
+mongoimport --db vod-development --collection equipment --file ./scripts/db-data/equipment.json --jsonArray
+mongoimport --db vod-development --collection ingredients --file ./scripts/db-data/ingredients.json --jsonArray
+mongoimport --db vod-development --collection learningpaths --file ./scripts/db-data/learningpaths.json --jsonArray
+mongoimport --db vod-development --collection chefs --file ./scripts/db-data/chefs.json --jsonArray
+mongoimport --db vod-development --collection tags --file ./scripts/db-data/tags.json --jsonArray
