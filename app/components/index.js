7c15a2638c04c7d8504b0015e458064305c835dd
ASJ <aserrajaumot@gmail.com>
2017-02-02 19:33:26 +0100

    Adding an app/index.js containing all the components

---
+++ app/components/index.js
@@ -0,0 +1,34 @@
+export RegisterForm from './Auth/RegisterForm'
+export SignInForm from './Auth/SignInForm'
+
+export CustomInput from './Form/CustomInput'
+export validations from './Form/validations' // is a component? I think is a utils
+
+export Header from './HeaderFooter/Header'
+export Footer from './HeaderFooter/Footer'
+export SecondaryHeader from './HeaderFooter/SecondaryHeader'
+
+export PrivacyPolicy from './LegalComponents/PrivacyPolicy'
+export Terms from './LegalComponents/Terms'
+
+// export CategoryList from './RecipeDisplay/DetailedDisplay/CategoryList'
+// export EquipmentList from './RecipeDisplay/DetailedDisplay/EquipmentList'
+// export IngredientList from './RecipeDisplay/DetailedDisplay/IngredientList'
+// export LearningPathList from './RecipeDisplay/DetailedDisplay/LearningPathList'
+export Recipe from './RecipeDisplay/DetailedDisplay/Recipe'
+// export RecipeComponentDetailed from './RecipeDisplay/DetailedDisplay/RecipeComponentDetailed'
+// export RecipeFromAPI from './RecipeDisplay/DetailedDisplay/RecipeFromAPI'
+// export SkillsLearntList from './RecipeDisplay/DetailedDisplay/SkillsLearntList'
+// export StepList from './RecipeDisplay/DetailedDisplay/StepList'
+// export TagList from './RecipeDisplay/DetailedDisplay/TagList'
+
+// export RecipeComponent from './RecipeDisplay/OverviewDisplay/RecipeComponent'
+export RecipeList from './RecipeDisplay/OverviewDisplay/RecipeList'
+// export RecipeListFromAPI from './RecipeDisplay/OverviewDisplay/RecipeListFromAPI'
+// export RecipeOverview from './RecipeDisplay/OverviewDisplay/RecipeOverview'
+
+// export Equipment from './RecipeDisplay/RecipeForm/Equipment'
+export RecipeCreateForm from './RecipeDisplay/RecipeForm/index'
+// export Ingredients from './RecipeDisplay/RecipeForm/Ingredients'
+// export SkillsLearnt from './RecipeDisplay/RecipeForm/SkillsLearnt'
+// export Steps from './RecipeDisplay/RecipeForm/Steps'
