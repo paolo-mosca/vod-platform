6a38efee057ccfde99a2db761487db1a2f674a37
Adam Goldman <adamgoldman7@gmail.com>
2017-04-08 12:36:16 +0800

    (recipeDetails): add server auth

--- src/server/api/recipes/recipesRoutes.js
+++ src/server/api/recipes/recipesRoutes.js
@@ -12,9 +12,7 @@ router.route('/')
   .get(controller.getList)
 
 router.route('/:id')
-  .get(controller.getItem)
-  // commented for easy testing as a guest, until user auth flow is implemented
-  // .get(auth.decodeToken, auth.getFreshUser, controller.getItem)
+  .get(auth.decodeToken, auth.getFreshUser, controller.getItem)
   .put(auth.decodeToken, auth.getFreshUser, auth.verifyAdmin, controller.updateItem)
   .delete(auth.decodeToken, auth.getFreshUser, auth.verifyAdmin, controller.deleteItem)
 
