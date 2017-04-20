f0ab1736f897b6a611f6e9a105e50d67908d2803
goldylucks <adamgoldman7@gmail.com>
2017-04-20 15:29:34 +0800

    refactor(old): remove old code folder

--- old/app/utils/form.js
+++
@@ -1,7 +0,0 @@
-export function getSelectOptions (items, valueField = 'id', labelField = 'name') {
-  if (!items) { return }
-  return items.map((item) => ({
-    value: item[valueField],
-    label: item[labelField]
-  }))
-}
--- old/server/config/development.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  // enabled logging for development
-  logging: true,
-  db: 'mongodb://localhost:27017/vod-development'
-  // db: 'mongodb://localhost/vod-development'
-};
